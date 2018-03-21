7446682c49cc8f5aba514f141d238b3322328a5b
Asa Katida <github@holomaplefeline.net>
2018-03-20 18:32:35 -0700

    Sketch solution from whiteboard and write failing tests.

---
+++ challenges/shift-array/shift_array.py
@@ -0,0 +1,16 @@
+def insertShiftArray(array, item):
+    count = 0
+    for _ in array:
+        count += 1
+    count += 1
+    pos = count // 2
+    output = [None for _ in range(count)]
+    insertCheck = False
+    for i in range(count):
+        if insertCheck:
+            output[i] = array[i - 1]
+        elif i == pos:
+            output[i] = item
+        else:
+            output[i] = array[i]
+    return output
