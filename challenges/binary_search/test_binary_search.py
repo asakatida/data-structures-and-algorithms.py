a165c7a51281e563372da8456cbd6a23503e1bc4
Asa Katida <github@holomaplefeline.net>
2018-03-21 14:00:48 -0700

    Initialize directory and setup testing plan.

---
+++ challenges/binary_search/test_binary_search.py
@@ -0,0 +1,25 @@
+from binary_search import binary_search
+
+
+def test_binary_search_empty_array():
+    assert binary_search([], 0) == -1
+
+
+def test_binary_search_not_found_in_short_array():
+    assert binary_search([1, 2, 3], 0) == -1
+
+
+def test_binary_search_found_at_begining():
+    assert binary_search([0, 1, 2, 3, 4, 5], 0) == 0
+
+
+def test_binary_search_found_at_end():
+    assert binary_search([0, 1, 3, 4, 5], 5) == 4
+
+
+def test_binary_search_found_at_middle_even():
+    assert binary_search([0, 1, 3, 5], 3) == 2
+
+
+def test_binary_search_found_at_middle_odd():
+    assert binary_search([1, 3, 5], 3) == 1
