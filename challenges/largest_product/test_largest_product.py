da741ac677c9677152c1f7e8e0a3f990f149d645
Adam Grandquist <grandquista@gmail.com>
2018-03-22 15:22:58 -0700

    Implement get initial an pass a test.

--- challenges/largest_product/test_largest_product.py
+++ challenges/largest_product/test_largest_product.py
@@ -1,5 +1,13 @@
 from .largest_product import largest_product
 
 
-def test_largest_product():
+def test_largest_product_empty_array():
     assert largest_product([]) == 0
+
+
+def test_largest_product_single_pair():
+    assert largest_product([(1, 2)]) == 2
+
+
+def test_largest_product_early_answer():
+    assert largest_product([(1, 2), (2, 2), (2, 1), (1, 1)]) == 4
