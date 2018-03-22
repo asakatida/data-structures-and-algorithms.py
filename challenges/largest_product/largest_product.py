f8185d8a7f0dfb9017a14852530a2a7fc0b500ee
Asa Katida <github@holomaplefeline.net>
2018-03-22 15:26:02 -0700

    Implement loop to solve last test.

--- challenges/largest_product/largest_product.py
+++ challenges/largest_product/largest_product.py
@@ -2,4 +2,8 @@ def largest_product(array):
     if not array:
         return 0
     largest = array[0][0] * array[0][1]
+    for i in range(1, len(array)):
+        prod = array[i][0] * array[i][1]
+        if prod > largest:
+            largest = prod
     return largest
