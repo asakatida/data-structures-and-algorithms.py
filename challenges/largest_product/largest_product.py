ad2d6f7992aaa7ef8c40642ecf7a531e1506da1e
Asa Katida <github@holomaplefeline.net>
2018-03-22 16:06:26 -0700

    Break out sections of algorithm.

--- challenges/largest_product/largest_product.py
+++ challenges/largest_product/largest_product.py
@@ -1,9 +1,35 @@
+def adjacent_positions(array, i, j):
+    if i + 1 < len(array):
+        yield array[i + 1][j]
+    if i - 1 > 0:
+        yield array[i - 1][j]
+    row = array[i]
+    if j + 1 < len(row):
+        yield row[j + 1]
+    if j - 1 > 0:
+        yield row[j - 1]
+
+
+def largest_adjacent_product(array, i, j):
+    target = array[i][j]
+    largest = 0
+    for adjacent in adjacent_positions(array, i, j):
+        prod = target * adjacent
+        print('largest_adjacent_product', i, j, prod)
+        if prod > largest:
+            largest = prod
+    return largest
+
+
 def largest_product(array):
     if not array:
         return 0
-    largest = array[0][0] * array[0][1]
-    for i in range(1, len(array)):
-        prod = array[i][0] * array[i][1]
-        if prod > largest:
-            largest = prod
+    largest = 0
+    for i in range(len(array)):
+        for j in range(i % 2, len(array[i]), 2):
+            prod = largest_adjacent_product(array, i, j)
+            print('largest_product', i, j, prod)
+            if prod > largest:
+                largest = prod
+    print('largest_product', largest)
     return largest
