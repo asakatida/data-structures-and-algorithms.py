8d1b6eebeadb5e7bea2ddb85f6f59ee1bc7f86aa
Adam Grandquist <grandquista@gmail.com>
2018-03-23 15:17:06 -0700

    Stub out node implementation.

---
+++ data_structures/linked_list/node.py
@@ -0,0 +1,10 @@
+class Node:
+    def __init__(self, value, _next):
+        self.value = value
+        self._next = _next
+
+    def __repr__(self):
+        pass
+
+    def __str__(self):
+        pass
