d6c8544ef2fb6b04d7c31a2b061ceb8276f6a6c3
Asa Katida <github@holomaplefeline.net>
2018-03-23 15:31:52 -0700

    Test chained node.

--- data_structures/linked_list/test_node.py
+++ data_structures/linked_list/test_node.py
@@ -3,5 +3,15 @@ def test_empty_node(new_node):
     assert new_node._next is None
 
 
+def test_chained_node(chained_node):
+    assert chained_node.value is 1
+    assert chained_node._next is not None
+
+
 def test_node_repr(new_node):
     assert repr(new_node) == 'None'
+
+
+def test_node_repr_chain(chained_node):
+    assert repr(chained_node) == '1'
+    assert repr(chained_node._next) == '2'
