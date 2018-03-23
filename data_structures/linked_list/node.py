05c646069ae31b8c77e131a314cb43b599433f89
Adam Grandquist <grandquista@gmail.com>
2018-03-23 16:55:32 -0700

    Document and test formatting methods.

--- data_structures/linked_list/node.py
+++ data_structures/linked_list/node.py
@@ -1,10 +1,25 @@
 class Node:
     def __init__(self, value, _next=None):
+        """
+        Initialize new Node with optional next Node.
+        """
         self.value = value
         self._next = _next
 
     def __repr__(self):
-        return repr(self.value)
+        """
+        Return a formatted string representing Node.
+        """
+        if self._next is None:
+            return f'Node({ self.value !r})'
+        return f'Node({ self.value !r}, { self._next !r})'
 
     def __str__(self):
-        return str(self.value)
+        """
+        Return a string representing Node.
+        """
+        return f'''({
+            self.value
+        }, {
+            "<END>"if self._next is None else "..."
+        })'''
