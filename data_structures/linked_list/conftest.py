6d67a54a9a986f7a4dde9a87d38bf12e62f5a8ba
Asa Katida <github@holomaplefeline.net>
2018-03-23 15:14:03 -0700

    Stub out linked list implementation.

---
+++ data_structures/linked_list/conftest.py
@@ -0,0 +1,17 @@
+from linked_list import LinkedList
+import pytest
+
+
+@pytest.fixture
+def new_list():
+    return LinkedList()
+
+
+@pytest.fixture
+def ordered_list():
+    return LinkedList(range(3, 40, 3))
+
+
+@pytest.fixture
+def unordered_list():
+    return LinkedList(map(lambda i: i % 7, range(73, 40, -2)))
---
+++ data_structures/linked_list/linked_list.py
@@ -0,0 +1,104 @@
+class LinkedList:
+    def __init__(iterable=()):
+        pass
+
+    def __add__(self, value):
+        pass
+
+    def __contains__(self, key):
+        pass
+
+    def __delitem__(self, key):
+        pass
+
+    def __eq__(self, value):
+        pass
+
+    def __ge__(self, value):
+        pass
+
+    def __getattribute__(self, name):
+        pass
+
+    def __getitem__(self):
+        pass
+
+    def __gt__(self, value):
+        pass
+
+    def __iadd__(self, value):
+        pass
+
+    def __imul__(self, value):
+        pass
+
+    def __iter__(self):
+        pass
+
+    def __le__(self, value):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __lt__(self, value):
+        pass
+
+    def __mul__(self, value):
+        pass
+
+    def __ne__(self, value):
+        pass
+
+    def __repr__(self):
+        pass
+
+    def __reversed__(self):
+        pass
+
+    def __rmul__(self, value):
+        pass
+
+    def __setitem__(self, key, value):
+        pass
+
+    def __str__(self):
+        pass
+
+    def append(self, value):
+        pass
+
+    def clear(self):
+        pass
+
+    def copy(self):
+        pass
+
+    def count(self, value):
+        pass
+
+    def extend(self, it):
+        pass
+
+    def find(self, value):
+        pass
+
+    def index(self, value, start=0, stop=-1):
+        pass
+
+    def insert(self, index, value):
+        pass
+
+    def pop(self, index=0):
+        pass
+
+    def remove(self, value):
+        pass
+
+    def reverse(self):
+        pass
+
+    def sort(self):
+        pass
+
+    __hash__ = None
