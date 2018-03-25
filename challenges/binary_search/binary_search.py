c63d7c8730b953c6ff9d28ef36e3f2b32bb3bb07
Adam Grandquist <grandquista@gmail.com>
2018-03-24 18:59:43 -0700

    Test binary_search on single element arrays.

--- challenges/binary_search/binary_search.py
+++ challenges/binary_search/binary_search.py
@@ -1,15 +1,13 @@
 def binary_search(array, key):
-    bound_high = len(array)
+    bound_high = len(array) - 1
     bound_low = 0
     index = 0
-    while True:
-        new_pos = bound_low + (bound_high - bound_low) // 2
-        if new_pos == index:
-            return -1
-        index = new_pos
+    while bound_low <= bound_high:
+        index = bound_low + (bound_high - bound_low) // 2
         if array[index] == key:
             return index
         elif array[index] < key:
             bound_low = index + 1
         else:
             bound_high = index - 1
+    return -1
