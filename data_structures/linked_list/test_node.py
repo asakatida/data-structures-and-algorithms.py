74e9a1f69f10c3c01d82709861bb4508370d9909
Asa Katida <github@holomaplefeline.net>
2018-03-29 14:59:24 -0700

    Refactor to fix Node::__repr__.

--- data_structures/linked_list/test_node.py
+++ data_structures/linked_list/test_node.py
@@ -8,17 +8,17 @@ def test_chained_node(chained_node):
     assert chained_node._next is not None
 
 
-# def test_node_repr(new_node):
-#     assert repr(new_node) == 'Node(None)'
+def test_node_repr(new_node):
+    assert repr(new_node) == 'Node(None)'
 
 
 def test_node_str(new_node):
     assert str(new_node) == '(None, <END>)'
 
 
-# def test_node_repr_chain(chained_node):
-#     assert repr(chained_node) == 'Node(1, Node(2, Node(3)))'
-#     assert repr(chained_node._next) == 'Node(2, Node(3))'
+def test_node_repr_chain(chained_node):
+    assert repr(chained_node) == 'Node(1, Node(2, Node(3)))'
+    assert repr(chained_node._next) == 'Node(2, Node(3))'
 
 
 def test_node_str_chain(chained_node):
