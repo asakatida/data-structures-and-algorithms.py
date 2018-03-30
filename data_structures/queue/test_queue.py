fad51c7b5df8eace6f144ce314696e0119c7e932
Adam Grandquist <grandquista@gmail.com>
2018-03-30 15:49:53 -0700

    Start testing queue methods.

--- data_structures/queue/test_queue.py
+++ data_structures/queue/test_queue.py
@@ -1,2 +1,15 @@
+import pytest
+
+
 def test_empty_queue_default(new_queue):
     assert new_queue.head is None
+
+
+def test_empty_queue_dequeue(new_queue):
+    with pytest.raises(IndexError):
+        new_queue.dequeue()
+
+
+def test_empty_queue_enqueue(new_queue):
+    new_queue.enqueue(0)
+    assert new_queue.head.value == 0
