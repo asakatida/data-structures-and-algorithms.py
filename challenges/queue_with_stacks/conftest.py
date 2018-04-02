cc2a3cc6e4f47970ae65ef24a8c66e5802515ede
Adam Grandquist <grandquista@gmail.com>
2018-04-02 14:25:18 -0700

    Stub out methods and copy tests.

--- challenges/queue_with_stacks/conftest.py
+++ challenges/queue_with_stacks/conftest.py
@@ -1,5 +1,28 @@
 from .queue_with_stacks import Queue
+import pytest
 
 
-def queue_empty():
+@pytest.fixture
+def new_queue():
     return Queue()
+
+
+@pytest.fixture
+def ordered_queue():
+    queue = Queue()
+    set(map(queue.enqueue, range(3, 40, 3)))
+    return queue
+
+
+@pytest.fixture
+def unordered_queue():
+    queue = Queue()
+    set(map(queue.enqueue, map(lambda i: i % 7, range(73, 40, -2))))
+    return queue
+
+
+@pytest.fixture
+def large_queue():
+    queue = Queue()
+    set(map(queue.enqueue, range(0xFFFFFF)))
+    return queue
---
+++ challenges/queue_with_stacks/test_queue_with_stacks.py
@@ -0,0 +1,59 @@
+import pytest
+
+
+def test_empty_queue_default(new_queue):
+    assert new_queue.head is None
+
+
+def test_empty_queue_dequeue(new_queue):
+    with pytest.raises(IndexError):
+        new_queue.dequeue()
+
+
+def test_empty_queue_has_size(new_queue):
+    assert len(new_queue) == 0
+
+
+def test_data_queue_dequeue_changes_size(ordered_queue):
+    assert len(ordered_queue) == 13
+    assert ordered_queue.dequeue() == 3
+    assert len(ordered_queue) == 12
+
+
+def test_data_queue_dequeue(ordered_queue):
+    assert ordered_queue.dequeue() == 3
+    assert ordered_queue.dequeue() == 6
+
+
+def test_data_queue_dequeue_exaust(ordered_queue):
+    while ordered_queue:
+        ordered_queue.dequeue()
+    assert len(ordered_queue) == 0
+    with pytest.raises(IndexError):
+        ordered_queue.dequeue()
+
+
+def test_unordered_dequeue(unordered_queue):
+    assert unordered_queue.dequeue() == 3
+    assert unordered_queue.dequeue() == 1
+    assert unordered_queue.dequeue() == 6
+    assert unordered_queue.dequeue() == 4
+
+
+def test_empty_queue_enqueue(new_queue):
+    new_queue.enqueue(0)
+    assert new_queue.head.value == 0
+
+
+def test_empty_queue_enqueue_multiple(new_queue):
+    for _ in range(30):
+        new_queue.enqueue(0)
+    new_queue.enqueue(1)
+    assert len(new_queue) == 31
+    assert new_queue.dequeue() == 0
+
+
+def test_empty_queue_enqueue_changes_size(new_queue):
+    assert len(new_queue) == 0
+    new_queue.enqueue('test')
+    assert len(new_queue) == 1
