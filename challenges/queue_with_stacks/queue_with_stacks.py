79e863ec1aad872c312533904ff5d9ea6e0c4678
Asa Katida <github@holomaplefeline.net>
2018-04-02 14:25:18 -0700

    Stub out methods and copy tests.

--- challenges/queue_with_stacks/queue_with_stacks.py
+++ challenges/queue_with_stacks/queue_with_stacks.py
@@ -2,4 +2,21 @@ from .stack import Stack
 
 
 class Queue:
-    pass
+    def __init__(self):
+        self.left = Stack()
+        self.right = Stack()
+
+    def __len__(self):
+        return len(self.left) + len(self.right)
+
+    def __repr__(self):
+        return ''
+
+    def __str__(self):
+        return ''
+
+    def dequeue(self):
+        pass
+
+    def enqueue(self, value):
+        pass
