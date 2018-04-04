06727e836b4ba65df4b833380b7eef846535d6e0
Asa Katida <github@holomaplefeline.net>
2018-04-04 14:42:04 -0700

    Sketch code from whiteboard.

---
+++ challenges/multi_bracket_validation/multi_bracket_validation.py
@@ -0,0 +1,18 @@
+from .stack import Stack
+
+
+def multi_bracket_validation(input):
+    openers = Stack()
+    for c in input:
+        if c in '[({':
+            openers.push(c)
+        elif c == ']':
+            if openers.pop() != '[':
+                return False
+        elif c == ')':
+            if openers.pop() != '(':
+                return False
+        elif c == '}':
+            if openers.pop() != '{':
+                return False
+    return not openers
