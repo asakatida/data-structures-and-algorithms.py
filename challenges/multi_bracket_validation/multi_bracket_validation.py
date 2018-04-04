c61b3f1f50ae8cf8ab45b58f32d6874759a6cd02
Adam Grandquist <grandquista@gmail.com>
2018-04-04 14:47:12 -0700

    Expand bracket testing.

--- challenges/multi_bracket_validation/multi_bracket_validation.py
+++ challenges/multi_bracket_validation/multi_bracket_validation.py
@@ -7,12 +7,18 @@ def multi_bracket_validation(input):
         if c in '[({':
             openers.push(c)
         elif c == ']':
+            if not openers:
+                return False
             if openers.pop() != '[':
                 return False
         elif c == ')':
+            if not openers:
+                return False
             if openers.pop() != '(':
                 return False
         elif c == '}':
+            if not openers:
+                return False
             if openers.pop() != '{':
                 return False
     return not openers
