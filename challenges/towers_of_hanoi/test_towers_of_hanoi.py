d1e461e33f3110b6f4937c1c86dccdc16bcc5dd0
Asa Katida <github@holomaplefeline.net>
2018-04-05 14:48:24 -0700

    Test algorithm from whiteboard.

--- challenges/towers_of_hanoi/test_towers_of_hanoi.py
+++ challenges/towers_of_hanoi/test_towers_of_hanoi.py
@@ -1,1 +1,9 @@
 from .towers_of_hanoi import towers_of_hanoi
+
+
+def test_towers_of_hanoi_1():
+    assert len(list(towers_of_hanoi(1))) == 1
+
+
+def test_towers_of_hanoi_2():
+    assert len(list(towers_of_hanoi(2))) == 3
