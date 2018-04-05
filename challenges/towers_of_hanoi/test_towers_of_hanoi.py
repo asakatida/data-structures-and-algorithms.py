1508c2cd66262e8ab5ee857d9b4d5e63b4299722
Asa Katida <github@holomaplefeline.net>
2018-04-05 14:54:19 -0700

    Expand towers of hanoi testing.

--- challenges/towers_of_hanoi/test_towers_of_hanoi.py
+++ challenges/towers_of_hanoi/test_towers_of_hanoi.py
@@ -7,3 +7,11 @@ def test_towers_of_hanoi_1():
 
 def test_towers_of_hanoi_2():
     assert len(list(towers_of_hanoi(2))) == 3
+
+
+def test_towers_of_hanoi_3():
+    assert len(list(towers_of_hanoi(3))) == 7
+
+
+def test_towers_of_hanoi_4():
+    assert len(list(towers_of_hanoi(4))) == 15
