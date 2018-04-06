2d2b0933952099788aa195f50cce88270863fb7d
Adam Grandquist <grandquista@gmail.com>
2018-04-06 09:11:11 -0700

    Make list output.

--- challenges/towers_of_hanoi/towers_of_hanoi.py
+++ challenges/towers_of_hanoi/towers_of_hanoi.py
@@ -1,10 +1,31 @@
+from sys import argv
+
+
 def towers_of_hanoi(n, start='A', end='C', spare='B'):
     """
     Generate the steps of towers of hanoi.
     """
+    output = []
     if n == 1:
-        yield f'Disk { n } moved from { start } to { end }'
+        output.append(f'Disk { n } moved from { start } to { end }')
     else:
-        yield from towers_of_hanoi(n - 1, start, spare, end)
-        yield f'Disk { n } moved from { start } to { end }'
-        yield from towers_of_hanoi(n - 1, spare, end, start)
+        output.extend(towers_of_hanoi(n - 1, start, spare, end))
+        output.append(f'Disk { n } moved from { start } to { end }')
+        output.extend(towers_of_hanoi(n - 1, spare, end, start))
+    return output
+
+
+def main():
+    maximum = -1
+    for move in towers_of_hanoi(int(argv[1])):
+        m = int(move.split()[1])
+        if m > maximum:
+            maximum = m
+            print(move)
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print()
