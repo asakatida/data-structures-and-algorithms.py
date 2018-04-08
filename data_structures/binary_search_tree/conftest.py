0d4247280d351c87dc08da6d418c21fe8c7ae094
Asa Katida <github@holomaplefeline.net>
2018-04-07 17:02:44 -0700

    Clean out old tests.

--- data_structures/binary_search_tree/conftest.py
+++ data_structures/binary_search_tree/conftest.py
@@ -1,28 +1,12 @@
-from .linked_list import LinkedList
-from .node import Node
+from .bst import BST
 import pytest
 
 
 @pytest.fixture
-def new_list():
-    return LinkedList()
+def new_bst():
+    return BST()
 
 
 @pytest.fixture
-def ordered_list():
-    return LinkedList(range(3, 40, 3))
-
-
-@pytest.fixture
-def unordered_list():
-    return LinkedList(tuple(map(lambda i: i % 7, range(73, 40, -2))))
-
-
-@pytest.fixture
-def new_node():
-    return Node(None)
-
-
-@pytest.fixture
-def chained_node():
-    return Node(1, Node(2, Node(3)))
+def filled_bst():
+    return BST(i for j in range(4, 0, -1) for i in range(j, j * 4, j))
