18f62bca29cd3d0214a5e594199e3eac69429add
Asa Katida <github@holomaplefeline.net>
2018-04-07 21:43:57 -0700

    Improve test coverage.

--- data_structures/binary_search_tree/test_bst.py
+++ data_structures/binary_search_tree/test_bst.py
@@ -3,9 +3,17 @@ def test_empty_bst_length(new_bst):
     assert new_bst.root is None
 
 
+def test_data_bst_contains(filled_bst):
+    assert 12 in filled_bst
+    assert (7 in filled_bst) is False
+    assert 6 in filled_bst
+    assert 1 in filled_bst
+
+
 def test_empty_bst_insert(new_bst):
     new_bst.insert(1)
     assert new_bst.root is not None
+    assert 1 in new_bst
 
 
 def test_data_bst_has_length(filled_bst):
@@ -17,6 +25,12 @@ def test_data_bst_insert_changes_length(filled_bst):
     filled_bst.insert(-1)
     filled_bst.insert(-2)
     assert len(filled_bst) - start_len == 2
+    assert -2 in filled_bst
+
+
+def test_data_bst_insert_negative_left(filled_bst):
+    filled_bst.insert(-1)
+    assert filled_bst.root.left.left.left.left.value == -1
 
 
 def test_data_bst_in_order_traverse(filled_bst):
