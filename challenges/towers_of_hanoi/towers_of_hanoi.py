17aec1182413bfb8ee9381849f8ff1c035d9d458
Asa Katida <github@holomaplefeline.net>
2018-04-10 09:28:57 -0700

    Use iteration to solve towers of hanoi.

--- challenges/towers_of_hanoi/towers_of_hanoi.py
+++ challenges/towers_of_hanoi/towers_of_hanoi.py
@@ -1,10 +1,32 @@
-def towers_of_hanoi(n, start='A', end='C', spare='B'):
+from .stack import Stack
+
+
+class _Move:
+    def __init__(self, disk, start='A', end='C', spare='B'):
+        self.disk = disk
+        self.start = start
+        self.end = end
+        self.spare = spare
+
+    def __str__(self):
+        return f'Disk { self.disk } moved from { self.start } to { self.end }'
+
+
+def towers_of_hanoi(n):
     """
     Generate the steps of towers of hanoi.
     """
-    if n == 1:
-        yield f'Disk { n } moved from { start } to { end }'
-    else:
-        yield from towers_of_hanoi(n - 1, start, spare, end)
-        yield f'Disk { n } moved from { start } to { end }'
-        yield from towers_of_hanoi(n - 1, spare, end, start)
+    stack = Stack([_Move(n)])
+    while stack:
+        move = stack.pop()
+        if isinstance(move, _Move):
+            if move.disk == 1:
+                yield str(move)
+            else:
+                stack.push(
+                    _Move(move.disk - 1, move.spare, move.end, move.start))
+                stack.push(str(move))
+                stack.push(
+                    _Move(move.disk - 1, move.start, move.spare, move.end))
+        else:
+            yield move
