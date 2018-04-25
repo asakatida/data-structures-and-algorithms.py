e6a638ce77b06e055444d1cc5e785ef542ab4f94
Asa Katida <github@holomaplefeline.net>
2018-04-24 21:07:02 -0700

    Implement algorithm from whiteboard.

--- challenges/print_level_order/test_print_level_order.py
+++ challenges/print_level_order/test_print_level_order.py
@@ -1,36 +1,27 @@
 from .print_level_order import print_level_order
 
 
-# def test_empty_bst_fizzbuzztree_in_order_traverse(new_bst):
-#     new_bst = fizzbuzztree(new_bst)
-#     lst = []
-#     new_bst.in_order(lst.append)
-#     assert lst == []
-#
-#
-# def test_data_bst_fizzbuzztree_in_order_traverse(filled_bst):
-#     filled_bst = fizzbuzztree(filled_bst)
-#     lst = []
-#     filled_bst.in_order(lst.append)
-#     assert lst == ['1', '2', 'fizz', '4', 'fizz', '8', 'fizz', 'fizz']
-#
-#
-# def test_data_bst_fizzbuzztree_pre_order_traverse(filled_bst):
-#     filled_bst = fizzbuzztree(filled_bst)
-#     lst = []
-#     filled_bst.pre_order(lst.append)
-#     assert lst == ['4', 'fizz', '2', '1', '8', 'fizz', 'fizz', 'fizz']
-#
-#
-# def test_fizz_bst_fizzbuzztree_in_order_traverse(fizz_buzz_bst):
-#     fizz_buzz_bst = fizzbuzztree(fizz_buzz_bst)
-#     lst = []
-#     fizz_buzz_bst.in_order(lst.append)
-#     assert lst == ['fizzbuzz', 'fizz', 'buzz', '7', 'fizzbuzz', 'fizzbuzz', 'fizzbuzz']
-#
-#
-# def test_fizz_bst_fizzbuzztree_pre_order_traverse(fizz_buzz_bst):
-#     fizz_buzz_bst = fizzbuzztree(fizz_buzz_bst)
-#     lst = []
-#     fizz_buzz_bst.pre_order(lst.append)
-#     assert lst == ['fizzbuzz', 'fizz', 'fizzbuzz', 'buzz', '7', 'fizzbuzz', 'fizzbuzz']
+def test_empty_k_tree_print_level_order(new_k_tree):
+    assert print_level_order(new_k_tree) == '''\
+'''
+
+
+def test_data_k_tree_print_level_order(filled_k_tree):
+    assert print_level_order(filled_k_tree) == '''\
+1
+2 3 4
+5 6 7
+8 9'''
+
+
+def test_list_k_tree_print_level_order(linked_list_k_tree):
+    assert print_level_order(linked_list_k_tree) == '''\
+10
+4
+3
+2
+5
+7
+6
+9
+8'''
