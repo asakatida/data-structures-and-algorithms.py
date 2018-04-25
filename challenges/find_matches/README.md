e1277812500f1b09575d52b335faa8cd1c730e8e
Adam Grandquist <grandquista@gmail.com>
2018-04-25 13:53:36 -0700

    Clean out code for algorithm.

--- challenges/find_matches/README.md
+++ challenges/find_matches/README.md
@@ -1,11 +1,11 @@
-# Print level order
-Output a tree in level order with a line per level.
+# Find matching nodes
+Collect all nodes containing search value.
 
 ## Challenge
-Algorithm input consists of a k tree.
-Output is a string with a line of space separated items for each level.
+Algorithm input consists of a k tree, and a search value.
+Output is a collection of all nodes containing that value.
 No standard library functions are to be used.
 
 ## Solution
 Whiteboard Images
-- [27-print-level-order](../../assets/27-print-level-order.jpg)
+- [28-find-matches](../../assets/28-find-matches.jpg)
--- challenges/find_matches/test_find_matches.py
+++ challenges/find_matches/test_find_matches.py
@@ -1,27 +1,13 @@
-from .print_level_order import print_level_order
+from .find_matches import find_matches
 
 
-def test_empty_k_tree_print_level_order(new_k_tree):
-    assert print_level_order(new_k_tree) == '''\
-'''
+def test_empty_k_tree_find_matches(new_k_tree):
+    assert find_matches(new_k_tree) == []
 
 
-def test_data_k_tree_print_level_order(filled_k_tree):
-    assert print_level_order(filled_k_tree) == '''\
-1
-2 3 4
-5 6 7
-8 9'''
+def test_data_k_tree_find_matches(filled_k_tree):
+    assert find_matches(filled_k_tree) == []
 
 
-def test_list_k_tree_print_level_order(linked_list_k_tree):
-    assert print_level_order(linked_list_k_tree) == '''\
-10
-4
-3
-2
-5
-7
-6
-9
-8'''
+def test_list_k_tree_find_matches(linked_list_k_tree):
+    assert find_matches(linked_list_k_tree) == []
