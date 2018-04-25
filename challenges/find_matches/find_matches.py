0dcadfb84eaa69f70530bb714e1142b8c2709e7d
Asa Katida <github@holomaplefeline.net>
2018-04-25 13:40:18 -0700

    Initialize file structure.

---
+++ challenges/find_matches/find_matches.py
@@ -0,0 +1,24 @@
+from .queue import Queue
+
+
+def print_level_order(tree):
+    """
+    Output string with a line per level of the tree.
+    """
+    if not tree.root:
+        return ''
+    queue = Queue([tree.root])
+    next_queue = Queue()
+    output = [[]]
+    while queue or next_queue:
+        if not queue:
+            queue, next_queue = next_queue, queue
+            output.append([])
+            continue
+        node = queue.dequeue()
+        output[-1].append(str(node.val))
+        child = node.child
+        while child:
+            next_queue.enqueue(child)
+            child = child.sibling
+    return '\n'.join(map(' '.join, output))
