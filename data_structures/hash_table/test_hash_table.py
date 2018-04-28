3f9b130dcc582d99b043f9e4007097214e4c7f01
Asa Katida <github@holomaplefeline.net>
2018-04-28 13:47:13 -0700

    Finish implementing HashTable::get.

--- data_structures/hash_table/test_hash_table.py
+++ data_structures/hash_table/test_hash_table.py
@@ -6,6 +6,13 @@ def test_empty_hash_table_length(new_hash_table):
     assert isinstance(new_hash_table.buckets, list)
 
 
+def test_empty_hash_table_get(new_hash_table):
+    with raises(KeyError):
+        new_hash_table.get('')
+    with raises(TypeError):
+        new_hash_table.get(0)
+
+
 def test_empty_hash_table_insert_repeat(new_hash_table):
     assert 'None' not in new_hash_table
     new_hash_table.set('None', 0)
@@ -18,25 +25,32 @@ def test_empty_hash_table_insert_repeat(new_hash_table):
 
 
 def test_data_hash_table_contains(filled_hash_table):
-    with raises(TypeError):
-        assert 9 in filled_hash_table
-    with raises(TypeError):
-        assert (12 in filled_hash_table) is False
-    with raises(TypeError):
-        assert 6 in filled_hash_table
-    with raises(TypeError):
-        assert 1 in filled_hash_table
+    assert '4' in filled_hash_table
+    assert ('9' in filled_hash_table) is False
+    assert '2' in filled_hash_table
+    assert '1' in filled_hash_table
 
 
 def test_empty_hash_table_set(new_hash_table):
     new_hash_table.set('None', 1)
     assert len(new_hash_table) > 0
-    with raises(TypeError):
-        assert 1 in new_hash_table
+    assert 'None' in new_hash_table
 
 
 def test_data_hash_table_has_length(filled_hash_table):
     assert len(filled_hash_table) > 0
+
+
+def test_data_hash_table_collision_does_not_update_size(filled_hash_table):
+    start_len = len(filled_hash_table)
+    filled_hash_table.set('1', -1)
+    assert len(filled_hash_table) == start_len
+
+
+def test_data_hash_table_remove_fail_does_not_update_size(filled_hash_table):
+    start_len = len(filled_hash_table)
+    filled_hash_table.remove('-1')
+    assert len(filled_hash_table) == start_len
 
 
 def test_data_hash_table_set_changes_length(filled_hash_table):
@@ -44,14 +58,25 @@ def test_data_hash_table_set_changes_length(filled_hash_table):
     filled_hash_table.set('11', -1)
     filled_hash_table.set('12', -2)
     assert len(filled_hash_table) - start_len == 2
-    with raises(TypeError):
-        assert -2 in filled_hash_table
+    assert '12' in filled_hash_table
+
+
+def test_data_hash_table_set_collision_get(filled_hash_table):
+    filled_hash_table.set('12', -2)
+    assert filled_hash_table.get('12') == -2
+    with raises(KeyError):
+        filled_hash_table.get('21')
+
+
+def test_data_hash_table_get(filled_hash_table):
+    assert filled_hash_table.get('5') == 8
+    with raises(KeyError):
+        filled_hash_table.get('-1')
 
 
 def test_data_hash_table_set_negative_left(filled_hash_table):
     filled_hash_table.set('None', -1)
-    with raises(TypeError):
-        assert -1 not in filled_hash_table
+    assert '-1' not in filled_hash_table
 
 
 def test_bad_hash_contains(bad_hash_table):
@@ -63,3 +88,12 @@ def test_bad_hash_remove(bad_hash_table):
     assert 'edba' in bad_hash_table
     bad_hash_table.remove('edba')
     assert 'edba' not in bad_hash_table
+    with raises(KeyError):
+        bad_hash_table.get('edba')
+
+
+def test_bad_hash_table_get(bad_hash_table):
+    assert bad_hash_table.get('abcd') == 0
+    assert bad_hash_table.get('edba') == 116
+    with raises(KeyError):
+        bad_hash_table.get('abcde')
