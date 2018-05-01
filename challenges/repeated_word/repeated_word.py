7a95fb67db98961deb912412c7727183ead90734
Asa Katida <github@holomaplefeline.net>
2018-04-30 22:02:09 -0700

    Make tests pass.

--- challenges/repeated_word/repeated_word.py
+++ challenges/repeated_word/repeated_word.py
@@ -9,6 +9,7 @@ def repeated_word(string):
     """
     hash_table = HashTable()
     for word in finditer(r'\w+', string.lower()):
+        word = word.group()
         if word in hash_table:
             return word
         hash_table.set(word, True)
--- challenges/repeated_word/test_repeated_word.py
+++ challenges/repeated_word/test_repeated_word.py
@@ -6,16 +6,20 @@ def test_empty_string_repeated_word():
 
 
 def test_no_repeat_repeated_word():
-    assert repeated_word('') is None
+    assert repeated_word('the quick brown fox') is None
 
 
 def test_repeat_start_repeated_word():
-    assert repeated_word('') is None
+    assert repeated_word('the the quick brown fox') == 'the'
 
 
 def test_repeat_end_repeated_word():
-    assert repeated_word('') is None
+    assert repeated_word('the quick brown fox fox') == 'fox'
 
 
 def test_repeat_middle_repeated_word():
-    assert repeated_word('') is None
+    assert repeated_word('the quick brown brown fox jumps') == 'brown'
+
+
+def test_repeat_chain_repeated_word():
+    assert repeated_word('the quick brownbrown fox jumps') is None
