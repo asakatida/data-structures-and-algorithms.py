e2f270ebeec970b6cce8d99e7b25169ad9b49c59
Asa Katida <github@holomaplefeline.net>
2018-04-30 21:59:44 -0700

    Implement algorithm from whiteboard.

--- challenges/repeated_word/repeated_word.py
+++ challenges/repeated_word/repeated_word.py
@@ -1,4 +1,14 @@
+from re import finditer
+
+from .hash_table import HashTable
+
+
 def repeated_word(string):
     """
     Output first repeated word.
     """
+    hash_table = HashTable()
+    for word in finditer(r'\w+', string.lower()):
+        if word in hash_table:
+            return word
+        hash_table.set(word, True)
