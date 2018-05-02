d472be84e73b65faedd94a3b93c85a64b8a35d27
Adam Grandquist <grandquista@gmail.com>
2018-05-01 17:04:22 -0700

    Implement algorithm from whiteboard.

--- challenges/tree_intersection/test_tree_intersection.py
+++ challenges/tree_intersection/test_tree_intersection.py
@@ -1,5 +1,19 @@
+from .bst import BST
 from .tree_intersection import tree_intersection
 
 
 def test_empty_trees():
-    assert tree_intersection() == set()
+    assert tree_intersection(BST(), BST()) == set()
+
+
+def test_repeated_val_trees():
+    left = BST(['5', '4', '2', '3', '1', '9', '6', '7', '8'])
+    right = BST(['4', '2', '6', '8'])
+    right.root.left = BST(['4', '2', '6', '8']).root
+    assert tree_intersection(left, right) == {'2', '4', '6', '8'}
+
+
+def test_no_intersection_trees():
+    assert tree_intersection(
+        BST(['3', '2', '1']),
+        BST(['4', '5', '6'])) == set()
--- challenges/tree_intersection/tree_intersection.py
+++ challenges/tree_intersection/tree_intersection.py
@@ -1,4 +1,31 @@
+from .hash_table import HashTable
+
+
 def tree_intersection(left, right):
     """
     Output the set of values common to two trees.
     """
+    def _recurse(node):
+        # handle empty tree
+        if node is None:
+            return HashTable()
+
+        # build sub trees
+        left_set = _recurse(node.left)
+        right_set = _recurse(node.right)
+
+        # get union of values
+        for value in left_set:
+            right_set.set(value, None)
+        right_set.set(node.value, None)
+        return right_set
+
+    # build tree sets
+    left_values = _recurse(left.root)
+    right_values = _recurse(right.root)
+
+    # get intersection of values
+    for value in left_values:
+        if value not in right_values:
+            left_values.remove(value)
+    return set(left_values)
