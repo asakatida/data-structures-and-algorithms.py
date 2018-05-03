39b1b4fd3062c1072b2d2a34036c2989879218fd
Asa Katida <github@holomaplefeline.net>
2018-05-02 17:52:16 -0700

    Implement algorithm from whiteboard.

--- challenges/left_join/left_join.py
+++ challenges/left_join/left_join.py
@@ -5,3 +5,10 @@ def left_join(left, right):
     """
     Join hash trees in a simplified left join.
     """
+    join = HashTable()
+    for key in left:
+        if key in right:
+            join.set(key, (left.get(key), right.get(key)))
+        else:
+            join.set(key, (left.get(key), None))
+    return join
