12313b345fc81bf7d31b28fc301662cb924f99a3
Adam Grandquist <grandquista@gmail.com>
2018-05-07 14:34:18 -0700

    Implement algorithm from whiteboard.

--- challenges/mergesort/mergesort.py
+++ challenges/mergesort/mergesort.py
@@ -2,3 +2,38 @@ def mergesort(array):
     """
     Sort an array using a recursive mergesort.
     """
+    _static = object()
+
+    def _merge_help(array, start, end):
+        nonlocal _static
+        if start >= end:
+            return
+        if start == end - 1:
+            yield array[start]
+            return
+        pivot = (start + end) // 2
+        left = _merge_help(array, start, pivot)
+        right = _merge_help(array, pivot, end)
+        i = j = _static
+        while True:
+            if i is _static:
+                try:
+                    i = next(left)
+                except StopIteration:
+                    yield from right
+                    return
+            if j is _static:
+                try:
+                    j = next(right)
+                except StopIteration:
+                    yield from left
+                    return
+            print(i, j)
+            if i < j:
+                yield i
+                i = _static
+            else:
+                yield j
+                j = _static
+
+    return list(_merge_help(array, 0, len(array)))
--- challenges/mergesort/test_mergesort.py
+++ challenges/mergesort/test_mergesort.py
@@ -1,5 +1,42 @@
+from random import shuffle
+
 from .mergesort import mergesort
 
 
+def mergesort_test(input):
+    output = mergesort(input)
+    return output, sorted(output)
+
+
 def test_empty_mergesort():
-    assert mergesort([]) == []
+    left, right = mergesort_test([])
+    assert left == right
+
+
+def test_small_mergesort():
+    left, right = mergesort_test([5, 3, 1, 4, 2])
+    assert left == right
+
+
+def test_invert_mergesort():
+    left, right = mergesort_test(list(range(10, 0, -1)))
+    assert left == right
+
+
+def test_large_mergesort():
+    left, right = mergesort_test([i for _ in range(1000) for i in range(3)])
+    assert left == right
+
+
+def test_sorted_mergesort():
+    left, right = mergesort_test(
+        [i for j in range(100) for i in range(j, j + 2)])
+    assert left == right
+
+
+def test_random_mergesort():
+    x = list(range(1000))
+    for _ in range(100):
+        shuffle(x)
+        left, right = mergesort_test(x)
+        assert left == right
