7c415963dc4393b94d96e4d55c74266172ec0cf2
Asa Katida <github@holomaplefeline.net>
2018-05-09 10:31:03 -0700

    Fix for mid pivot and duplicate values.

--- challenges/quicksort/quicksort.py
+++ challenges/quicksort/quicksort.py
@@ -3,26 +3,32 @@ def quicksort(array):
     Sort an array using a recursive quicksort.
     """
     def _quick_help(array, start, end):
-        pivot = end - 1
-        right = pivot - 1
-        left = start
-        if pivot <= left:
+        if start >= end:
             return array
-        while left < pivot:
-            if array[pivot] < array[right]:
-                array[pivot], array[right] = array[right], array[pivot]
+        right = end
+        pivot = (end + start) // 2
+        left = start
+        while left < pivot or pivot < right:
+            if left < pivot and array[pivot] < array[pivot - 1]:
+                array[pivot - 1], array[pivot] = array[pivot], array[pivot - 1]
                 pivot -= 1
-                right -= 1
-            elif array[pivot] < array[left]:
-                array[left], array[right] = array[right], array[left]
+            elif pivot < right and array[pivot + 1] < array[pivot]:
+                array[pivot + 1], array[pivot] = array[pivot], array[pivot + 1]
+                pivot += 1
+            elif left < pivot and array[pivot] < array[left]:
+                array[left], array[pivot - 1] = array[pivot - 1], array[left]
                 left += 1
+            elif pivot < right and array[right] < array[pivot]:
+                array[right], array[pivot + 1] = array[pivot + 1], array[right]
+                right -= 1
             else:
                 left += 1
+                right -= 1
         return _quick_help(
             _quick_help(
                 array,
-                start, pivot),
+                start, pivot - 1),
             pivot + 1, end)
 
     array = list(array)
-    return _quick_help(array, 0, len(array))
+    return _quick_help(array, 0, len(array) - 1)
