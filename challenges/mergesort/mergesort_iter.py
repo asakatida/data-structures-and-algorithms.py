28ca374fa7cf8c08ec91ee92bb20f4498c34ca4f
Adam Grandquist <grandquista@gmail.com>
2018-07-04 13:33:29 -0700

    Work on iterative merge sort.

--- challenges/mergesort/mergesort_iter.py
+++ challenges/mergesort/mergesort_iter.py
@@ -1,22 +1,33 @@
 from itertools import chain, islice, repeat, tee
 
 
+def sort_range(array, start, end):
+    """
+    Sort an array slice using an iterative mergesort.
+    """
+    mid = (start + end) // 2
+    ks = chain(range(mid, end), repeat(end))
+    k = next(ks)
+    for j in range(start, mid):
+        while array[k] < array[j]:
+            array[k], array[j] = array[j], array[k]
+            k = next(ks)
+
+
 def mergesort(array):
     """
     Sort an array using an iterative mergesort.
     """
     array = list(array)
     len_array = len(array)
-    max_index = repeat(len_array - 1)
+    max_index = len_array - 1
     step = 1
     while step < len_array:
-        odd, even = tee(range(0, len_array, step))
-        for s in zip(islice(odd, 0, None, 2), islice(even, 1, None, 2)):
-            ks = chain(range(s[1], len_array), max_index)
-            k = next(ks)
-            for j in range(*s):
-                if array[k] < array[j]:
-                    array[k], array[j] = array[j], array[k]
-                    k = next(ks)
+        steps = tee(chain(range(0, len_array, step), [max_index]))
+        odd = chain(islice(steps[0], 0, None, 2), [max_index])
+        even = chain(islice(steps[1], 1, None, 2), [max_index])
+        for start, end in zip(odd, even):
+            sort_range(array, start, end)
         step <<= 1
+    sort_range(array, 0, len_array)
     return array
